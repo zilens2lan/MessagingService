b170ecfd4fd317497455946c128cba983b41f501
ZilenS2Lan <Ripzil12>
2021-02-23 10:26:11 +0300

    delete all

--- src/main/java/com/zilen/messagingService/MessageApplication.java
+++ src/main/java/com/zilen/messagingService/MessageApplication.java
@@ -1,14 +1,10 @@
 package main.java.com.zilen.messagingService;
 
 
-import main.java.com.zilen.messagingService.service.MessagingService;
-import main.java.com.zilen.messagingService.service.serviceImpl.MessagingServiceImpl;
-
 public class MessageApplication {
 
     public static void main(String[] args) {
         System.out.println("start");
-        MessagingService messagingService = new MessagingServiceImpl();
-        messagingService.viewMessage();
+        //hello world
     }
 }

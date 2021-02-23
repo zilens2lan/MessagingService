ec276dae2e97187ff51ea057ec32df254580118e
ZilenS2Lan <Ripzil12>
2021-02-23 06:30:27 +0300

    test

--- src/main/java/com/zilen/messagingService/MessageApplication.java
+++ src/main/java/com/zilen/messagingService/MessageApplication.java
@@ -6,6 +6,7 @@ import main.java.com.zilen.messagingService.service.serviceImpl.MessagingService
 
 public class MessageApplication {
 
+    //helloworld
     public static void main(String[] args) {
         System.out.println("start");
         MessagingService messagingService = new MessagingServiceImpl();

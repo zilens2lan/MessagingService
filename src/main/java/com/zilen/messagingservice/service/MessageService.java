684632299dab162cfd42286e45bc7bfe3b4e3205
ZilenS2Lan <zilens2lan@mail.ru>
2021-04-15 11:01:35 +0300

    Refactoring.

--- src/main/java/com/zilen/messagingservice/service/MessageService.java
+++ src/main/java/com/zilen/messagingservice/service/MessageService.java
@@ -1,16 +1,8 @@
 package com.zilen.messagingservice.service;
 
 import com.zilen.messagingservice.entity.Message;
-import com.zilen.messagingservice.entity.attachment.Audio;
-import com.zilen.messagingservice.entity.attachment.Document;
-import com.zilen.messagingservice.entity.attachment.Picture;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.time.Duration;
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -20,35 +12,5 @@ public class MessageService {
 
     public void send(Message message) {
         messageRedirectingService.redirect(message);
-    }
-
-    public Message createMessage() {
-        Document document = Document.builder()
-                .id(UUID.randomUUID())
-                .title("document")
-                .format("doc")
-                .size(Byte.MAX_VALUE)
-                .build();
-        Picture picture = Picture.builder()
-                .id(UUID.randomUUID())
-                .title("picture")
-                .format("png")
-                .height(1080)
-                .width(1920)
-                .build();
-        Audio audio = Audio.builder()
-                .id(UUID.randomUUID())
-                .title("audio")
-                .format("mp3")
-                .duration(Duration.ofSeconds(364))
-                .build();
-
-        return Message.builder()
-                .id(UUID.randomUUID())
-                .userName("Zilen")
-                .text("Text Text Text")
-                .dateTime(LocalDateTime.now())
-                .attachments(List.of(document, picture, audio))
-                .build();
     }
 }
